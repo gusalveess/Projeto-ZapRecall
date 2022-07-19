0c3f4019de8d6bb41cfb397b7e6c6dfecb78391b
Gustavo <gustaalveess1302@gmail.com>
2022-07-19 17:01:33 -0300

    perguntas

--- src/Perguntas.js
+++ src/Perguntas.js
@@ -1,10 +1,34 @@
+import React, { useState } from 'react';
+
+function MenuPerguntas(props) {
+    return (
+        <div>
+            <div class="pergunta">
+                <p>{props.pergunta}</p>
+                <ion-icon name="play-outline"></ion-icon>
+            </div>
+        </div>
+    )
+}
+
 export default function Perguntas() {
+
+    const perguntas = [
+        { pergunta: "Pergunta 1" },
+        { pergunta: "Pergunta 2" },
+        { pergunta: "Pergunta 3" },
+        { pergunta: "Pergunta 4" }
+    ]
+
     return (
         <div>
             <div class="titleQuest">
-              <img className="logo-pequeno" src="imgs/logo-pequeno.svg"/>
-              <p>ZapRecall</p>
+                <img className="logo-pequeno" src="imgs/logo-pequeno.svg" />
+                <p>ZapRecall</p>
             </div>
+
+            {perguntas.map((item) => <MenuPerguntas pergunta={item.pergunta} />)}
+
             <div class="status">
                 <p>0/4 CONCLUÍDOS</p>
             </div>
