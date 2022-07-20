14c15bf6f629b671048d6bea44e64ee996aeb180
Gustavo <gustaalveess1302@gmail.com>
2022-07-20 16:47:26 -0300

    questions-one

--- src/Perguntas.js
+++ src/Perguntas.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 
 function MenuPerguntas(props) {
+
+    const [question, setPlay] = useState("pergunta");
+    const [icon, setRotate] = useState( <ion-icon onClick= {() => play()} name="play-outline"></ion-icon>)
+
+    function play() {
+        setPlay("quest")
+        setRotate(<img src="/imgs/rotate.svg"/>)
+    }
+
     return (
         <div>
-            <div class="pergunta">
+            <div class={question}>
                 <p>{props.pergunta}</p>
-                <ion-icon name="play-outline"></ion-icon>
+                {icon}
             </div>
         </div>
     )
@@ -18,6 +27,17 @@ export default function Perguntas() {
         { pergunta: "Pergunta 2" },
         { pergunta: "Pergunta 3" },
         { pergunta: "Pergunta 4" }
+    ]
+
+    const Flashcards = [
+        {Flashcard: "O que é JSX?"},
+        {Flashcard: "O React é __"},
+        {Flashcard: "Componentes devem iniciar com __"},
+        {Flashcard: "Podemos colocar __ dentro do JSX"},
+        {Flashcard: "O ReactDOM nos ajuda __ "},
+        {Flashcard: "Usamos o npm para __"},
+        {Flashcard: "Usamos props para __ "},
+        {Flashcard: "Usamos estado (state) para __"}
     ]
 
     return (
